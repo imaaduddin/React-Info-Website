89cb0568cff0a96119b477493387f599620ddd32
Imaad Uddin <imaaduddin27@gmail.com>
2024-11-24 01:13:27 -0600

    props and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,44 @@
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  let tabContent = "Please click a button";
+
+  function handleSelect(selectedButton) {
+    // selectedButton => "components", "jsx", "props", "state"
+    tabContent = selectedButton;
+  }
+
   return (
     <div>
       <Header/>
-
       <main>
         <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+          <CoreConcept 
+            title={CORE_CONCEPTS[0].title}
+            description={CORE_CONCEPTS[0].description}
+            image={CORE_CONCEPTS[0].image}
+          />
+          <CoreConcept {...CORE_CONCEPTS[1]}/>
+          <CoreConcept {...CORE_CONCEPTS[2]}/>
+          <CoreConcept {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")} label="Components"/>
+            <TabButton onSelect={() => handleSelect("jsx")} label="JSX"/>
+            <TabButton onSelect={() => handleSelect("props")} label="Props"/>
+            <TabButton onSelect={() => handleSelect("state")} label="State"/>
+          </menu>
+          {tabContent}
+        </section>
       </main>
     </div>
   );
